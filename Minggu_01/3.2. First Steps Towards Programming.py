ea316cb0894ef70109086e9d23355b571306a564
Figo de Fretes <90168368+Brogoxx@users.noreply.github.com>
2023-03-16 21:41:33 +0000

    0.4

---
+++ Minggu_01/3.2. First Steps Towards Programming.py
@@ -0,0 +1,16 @@
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
+
+print("================================")
+
+i = 256*256
+print('The value of i is', i)
+
+print("================================")
+
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a+b
